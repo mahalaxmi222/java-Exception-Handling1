9742c3e108205705a8d8dc94bb0afe8a2dc9c791
P.Mahalaxmi <150656303+mahalaxmi222@users.noreply.github.com>
2023-12-02 10:30:16 +0530

    Update ExceptionPropagationExample.java

--- Exception Handling/Object/Throwable/Exception/Java Exception Keywords and Blocks/Exception Propagation/1.Practice/ExceptionPropagationExample.java
+++ Exception Handling/Object/Throwable/Exception/Java Exception Keywords and Blocks/Exception Propagation/1.Practice/ExceptionPropagationExample.java
@@ -7,16 +7,38 @@ public class ExceptionPropagationExample {
         } catch (Exception e) {
             // Handling the exception at the top-level of the call stack
             System.err.println("Top-level exception handler: " + e.getMessage());
+        } finally {
+            // This block is always executed, whether an exception occurred or not
+            System.out.println("Finally block in the main method.");
         }
     }
 
     public static void method1() {
-        // Call another method that might throw an exception
-        method2();
+        try {
+            // Call another method that might throw an exception
+            method2();
+        } catch (Exception e) {
+            // Handle the exception or rethrow it if needed
+            System.err.println("Exception caught in method1: " + e.getMessage());
+            throw e; // Propagate the exception further
+        } finally {
+            // This block is always executed, whether an exception occurred or not
+            System.out.println("Finally block in method1.");
+        }
     }
 
     public static void method2() {
-        // Simulate an exception
-        int result = 10 / 0; // This will throw an ArithmeticException
+        try {
+            // Simulate an exception
+            int result = 10 / 0; // This will throw an ArithmeticException
+        } catch (ArithmeticException e) {
+            // Handle the exception or rethrow it if needed
+            System.err.println("ArithmeticException caught in method2: " + e.getMessage());
+            throw e; // Propagate the exception further
+        } finally {
+            // This block is always executed, whether an exception occurred or not
+            System.out.println("Finally block in method2.");
+        }
     }
 }
+
